c9e215b4a6cf301a001741222735e8af16266fb6
Gustavo Ornelas <ornelasg93@gmail.com>
2022-09-24 21:00:32 -0500

    Se continuan cambios de JS, sin embargo no funcionan los botones Edit/Reset y LocalSotrage,

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ function onFormSubmit(){
         insertNewRecord(formData);
     }
     else {
-
+        updateRecord(formData)
+     
     }
+    resetForm();
 }
 
 //Datos//
@@ -33,9 +35,45 @@ function insertNewRecord(data) {
         cell3.innerHTML = data.Cantidad;
     var cell4 = newRow.insertCell(3);
         cell4.innerHTML = data.Precio;  
-    var cell5 = newRow.insertCell(4);
-        cell5.innerHTML ="<button>Edit</button>  <button>Delete</button>" 
+    var cel15 = newRow.insertCell(4);
+        ce115.innerHTML ="<button>Edit)</button>  <button>Delete</button>"
+        
+}
 
+//Editar datos 
 
+function onEdit(td){
 
+    selectedRow = td.parentElement.parentElement;
+   document.getElementById ('codigoProducto').value = selectedRow.cells [0] .innerHTML;
+    document.getElementById ('Producto') .value = selectedRow.cells [1] .innerHTML;
+    document.getElementById ('Cantidad') .value = selectedRow.cells [2] .innerHTML;
+   document.getElementById ( 'Precio'). value selectedRow.cells [3] .innerHTML;
+
+}
+
+function updateRecord ( formData ) {
+    selectedRow.cells [0].innerHTML= codigoProducto;
+    selectedRow.cells [1].innerHTML= formData.Producto;
+    selectedRow.cells [2].innerHTML= formData.Precio;
+    selectedRow.cells [3].innerHTML= formData.Cantidad;
+
+}
+ 
+//Borrar datos
+
+function onDelete(td){
+    if (confirm("Seguro que quieres borrar?")){
+        row= td.parentElement.parentElement;
+        document.getElementById("storelist").deleteRow(row.rowIndex);
+    }
+}
+
+//Reset datos 
+
+function resetForm(){
+    document.getElementById("codigoProducto").value = "";
+    document.getElementById("Producto").value = "";
+    document.getElementById("Cantidad").value = "";
+    document.getElementById("Precio").value = "";
 }
